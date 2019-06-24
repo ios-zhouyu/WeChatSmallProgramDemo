a6d2375729485dc0b385fbbb04150f31d82ba64c
zhouyu <zhouyu@haodf.com>
2019-06-24 17:27:07 +0800

    组件solt的使用

---
+++ component/tableViewDefaultCell/tableViewDefaultCell.js
@@ -0,0 +1,34 @@
+// component/tableViewDefaultCell.js
+Component({
+    options: {
+        multipleSlots: true // 在组件定义时的选项中启用多slot支持
+    },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title: {
+            type: String,
+            value: null
+        },
+        subtitle: {
+            type: String,
+            value: null
+        }
+        
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
